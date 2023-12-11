369ac8e68df2dae9de44ffd01f127ea6ad205521
Tess Linden <tess.linden@gmail.com>
2023-12-11 12:59:04 -0500

    Added support for importing Amex BCP and Amex Plat transactions from csv transaction history files downloaded from Amex website.

--- budgetbuddy/config.py
+++ budgetbuddy/config.py
@@ -13,15 +13,31 @@ for path in [PATH_TO_BUDGET, PATH_TO_PLOTS, PATH_TO_TRANSACTIONS]:
 
 FOLDERS_LIST: List[Folder]= [
     Folder(
+        name='abcp', 
+        prefix='abcp', 
+        suffix='.csv', 
+        columns_to_rename={'description': 'transaction'}, 
+        account='Amex BCP', 
+        flip_sign_of_amounts=True, 
+        prune_columns=True, 
+    ),
+    Folder(
+        name='apla', 
+        prefix='apla', 
+        suffix='.csv', 
+        columns_to_rename={'description': 'transaction'}, 
+        account='Amex Plat', 
+        flip_sign_of_amounts=True, 
+        prune_columns=True, 
+    ),
+    Folder(
         name='asav',
         prefix='History',
         suffix='.csv',
         columns_to_rename={'description': 'transaction'},
         account='Alliant Savings',
         reformat_amounts_alliant=True,
-        flip_sign_of_amounts=False,
         prune_columns=True,
-        fill_false=False,
         filename_format=(
             r"History-(?P<month>\d{2})(?P<day>\d{2})(?P<year>\d{2})-" + 
             r"(?P<hour>\d{2})(?P<minute>\d{2})(?P<seconds>\d{2})"
@@ -36,7 +52,6 @@ FOLDERS_LIST: List[Folder]= [
         reformat_amounts_alliant=True, 
         flip_sign_of_amounts=True, 
         prune_columns=True, 
-        fill_false=False,
         filename_format=(
             r"History-(?P<month>\d{2})(?P<day>\d{2})(?P<year>\d{2})-" +
             r"(?P<hour>\d{2})(?P<minute>\d{2})(?P<seconds>\d{2})"
@@ -52,13 +67,11 @@ FOLDERS_LIST: List[Folder]= [
         name='merged',
         prefix='merged',
         suffix='.csv',
-        fill_false=False,
     ),
     Folder(
         name='relay',
         prefix='relay',
         suffix='.txt',
-        fill_false=False,
     ),
     Folder(
         name='usbac',
@@ -66,9 +79,7 @@ FOLDERS_LIST: List[Folder]= [
         suffix='.csv',
         columns_to_rename={'transaction': 'transaction_type', 'name': 'transaction',},
         account='USB_AC',
-        flip_sign_of_amounts=False,
         prune_columns=True,
-        fill_false=False,
         filename_format=(
             r"USB AC - 5327_\d{2}-\d{2}-\d{4}_(?P<month>\d{2})-(?P<day>\d{2})-" + 
             r"(?P<year>\d{4})(?P<hour>)(?P<minute>)(?P<seconds>)"
