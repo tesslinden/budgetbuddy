eb2c7666940be1e0dd730f47b0b8003a0e66df78
Tess Linden <tess.linden@gmail.com>
2024-01-18 15:40:09 -0800

    added some spaces and a line break for cleaner formatting

--- budgetbuddy/utils/misc.py
+++ budgetbuddy/utils/misc.py
@@ -3,9 +3,10 @@ from datetime import datetime
 
 import re
 
-def get_arguments(function, locals):#not sure what types to put for these
+def get_arguments(function, locals): # not sure what types to put for these arguments
     """Returns a dictionary of the argument names and their current values for a given function.
-    Always pass locals() as the second argument to get_arguments()."""
+    Always pass locals() as the second argument to get_arguments().
+    """
     # argument_names = function.__code__.co_varnames[:function.__code__.co_argcount] # this might also work
     argument_names = list(inspect.signature(function).parameters.keys())
     argument_values = [locals[argument_name] for argument_name in argument_names if argument_name in locals.keys()]
